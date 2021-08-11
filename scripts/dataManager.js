bcadfd7ae34b143c003555d34775b30f9803930c
Yohan Lopes <yohanhxh@gmail.com>
2021-08-11 11:47:03 -0300

    adicionando funções de manipulação de dados locais

---
+++ scripts/dataManager.js
@@ -0,0 +1,23 @@
+
+async function getServerQuizzes() {
+    const response = await GLOBAL.api.get("/").catch(error => {
+        console.log("Erro na requisição");
+        console.log(error);
+        return false;
+    })
+    GLOBAL.serverQuizzes = response.data;
+}
+
+function loadLocallyStoragedQuizzes() {
+    const localQuizzes = localStorage.getItem("BuzzQuizz");
+    if(localQuizzes) {
+        GLOBAL.usersQuizzesIds = JSON.parse(localQuizzes);
+    } else {
+        GLOBAL.usersQuizzesIds = [];
+    }
+}
+
+function storeUsersQuizzesLocally() {
+    const storeQuizzes = JSON.stringify(GLOBAL.usersQuizzesIds);
+    localStorage.setItem("BuzzQuizz", storeQuizzes);
+}
--- scripts/globals.js
+++ scripts/globals.js
@@ -1,5 +1,7 @@
 const GLOBAL = {
-    api = axios.create({
+    api: axios.create({
         baseURL: "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes"
-    })
+    }),
+    serverQuizzes: [], //this is an array of objects
+    usersQuizzesIds: [], //this is an array os id strings
 }
