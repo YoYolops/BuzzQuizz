1e1d1debd7f87299f555bc3644e69625fcc22129
Yohan Lopes <yohanhxh@gmail.com>
2021-08-15 16:45:44 -0300

    adicionando funçao de verificacao de validade de cor hexadecimal

--- scripts/dataManager.js
+++ scripts/dataManager.js
@@ -74,6 +74,24 @@ function resetSecondScreenInterface() {
 }
 
 
+function isValidHexadecimalColor(hexadecimalString) {
+    const validCharacters = ["a", "b", "c", "d", "e", "f","1","2","3","4","5","6","7","8","9"];
+
+    for(let i = 1; i < hexadecimalString.length; i++) {
+        if(!validCharacters.includes(hexadecimalString[i].toLowerCase())) return false;
+    }
+
+    const conditions = [
+        hexadecimalString.length === 4 || hexadecimalString.length === 7,
+        hexadecimalString[0] === '#',
+    ]
+
+    for(condition of conditions) { if(!condition) return false; }
+
+    return true;
+}
+
+
 function savingBasicQuizzInformation () {
     const findBasicInformation = document.querySelector("#basic-information-quizz-screen .main-step-quizz ");
 
